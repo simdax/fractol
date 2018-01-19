346b2171aa036d0aae6434803b701796182e4cad
Simon CORNAZ <scornaz@e1r2p1.42.fr>
2018-01-19 10:15:18 +0100

    changed elem col map

--- srcs/shapes.c
+++ srcs/shapes.c
@@ -6,33 +6,40 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/27 12:49:25 by scornaz           #+#    #+#             */
-/*   Updated: 2018/01/19 09:48:45 by scornaz          ###   ########.fr       */
+/*   Updated: 2018/01/19 10:09:36 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+static float 	ft_abs(float a)
+{
+	return (a < 0 ? -a : a);
+}
 
 void	draw_line(t_line line, int reverse, t_color color, t_libx *libx)
 {
 	float x;
 	float y;
 	float z_slope;
-	float z;
+	float z1;
+	float z2;
 	
 	y = 0;
 	x = 0;
-	z = line.goal.z * 20;
-	z_slope = line.goal.z - line.origin.z;
+	z1 = line.origin.z * 5;
+	z2 = line.goal.z * 5;
+	z_slope = (z2 - z1) * 0.02;
 	while (x != line.steps)
 	{
 		if (reverse)
 			mlx_pixel_put(libx->mlx, libx->win,
 						line.origin.x + y, line.origin.y + x,
-						  color2hex(update(color, z + x * z_slope * 0.1, 0, 0)));
+						  color2hex(update(color, z1 + (x * z_slope), 0, 0)));
 		else
 			mlx_pixel_put(libx->mlx, libx->win,
 						line.origin.x + x, line.origin.y + y,
-						  color2hex(update(color, z + x * z_slope * 0.1, 0, 0)));
+						  color2hex(update(color, z2 + (x * z_slope), 0, 0)));
 		x += line.steps > 0 ? 1 : -1;
 		y += line.slope;
 	}
